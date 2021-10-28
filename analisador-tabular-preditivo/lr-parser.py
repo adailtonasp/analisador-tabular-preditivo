40dc3d34df49f2d74540e3642ace2e486a938e68
Felipe Barros <bcfelipe@icloud.com>
2021-10-28 17:44:12 -0300

    Organizar e documentar o código

--- analisador-tabular-preditivo/lr-parser.py
+++ analisador-tabular-preditivo/lr-parser.py
@@ -1,62 +1,194 @@
+# Trabalho de Implementação - Construção de Compiladores - 2021
+# Adailton Palhano, Felipe Barros e Maria Theresa.
+import os
 
-fita = input("Digite a sentença a ser avaliada: ")
-pilha = []
-simbolo = []
+goAhead = False  # indicar se o usuário deve prosseguir
 
-def vAct(a,s): 
-    index = actions.index(a)
-    listaAcoes = actionsTable[s]
-    acao = listaAcoes[index] #retorna o indice onde se encontrou o terminal
-    return acao
+# Variáveis utilizadas no programa
+countStates = 0 # Quantidade de Estados
+countActions = 0 # Quantidade de Ações
+countTransitions = 0 # Quantidade de Transições
+grammar = [] # Gramática inserida pelo Usuário
+actionsTable = []  # Tabela de Ações
+actions = []  # Variáveis das Ações
+sentence = "" # Sentença a ser analisada
+statesStack = [] # Pilha de estados
+simbolsStack = [] # Pilha de símbolos já processados
 
-#empilha o estado inicial
-pilha.append('0')
+# 1. Entrada dos Dados iniciais
+while(not goAhead):
+    print("------------ Preenchimento dos Dados Iniciais ------------")
+    try:
+        countStates = int(input("Insira a quantidade de estados: "))
+        countStates = range(countStates)
 
-a = fita[0]
+        countActions = int(input("Insira a quantidade de ações: "))
+
+        countTransitions = int(input("Insira a quantidade de transições: "))
+
+        countRules = int(input("Insira a quantidade de regras da gramática: "))
+
+        goAhead = input(
+            "\nPressione 1 para continuar. Qualquer outra tecla para voltar: ")
+        goAhead = goAhead == "1"
+    except:
+        input("\nVocê inseriu algum dado incorreto. Pressione qualquer tecla para repetir: ")
+        goAhead = False
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# 2. Preenchimento da tabela
+# - Dividimos o preenchimento da tabela em duas partes: tabela de ações e tabela de transições.
+# - A entrada de cada linha, em ambos os casos, consiste nos elementos separados por vírgula.
+# - Entrada com '@' siginifca uma ação ou transição vazia.
+
+# 2.1. Preenchimento da tabela de ações
+goAhead = False  # Indicar se o usuário deve prosseguir
+
+while(not goAhead):
+    print("------- Preenchimento dos Dados da Tabela de Ações -------")
+    print("* entradas separadas por vírgula. @ para entradas vazias *\n")
+    try:
+        actions = input(
+            f"Insira os variáveis({countActions}) das ações: ").split(',')
+
+        actionLine = []
+        for i in range(countStates):
+            actionLine = input(f"Insira as ações({countActions}) da linha {i+1}: ")
+            actionLine = actionLine.split(',')
+            actionsTable.append(actionLine)
+
+        goAhead = input(
+            "\nPressione 1 para continuar. Qualquer outra tecla para repetir: ")
+        goAhead = goAhead == "1"
+    except:
+        input("\nVocê inseriu algum dado incorreto. Pressione qualquer tecla para repetir: ")
+        goAhead = False
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# 2.2. Preenchimento da tabela de transições
+# - Dividimos o preenchimento da tabela em duas partes: tabela de ações e tabela de transições.
+# - A entrada de cada linha, em ambos os casos, consiste nos elementos separados por vírgula.
+# - Entrada com '@' siginifca uma ação ou transição vazia.
+
+goAhead = False  # Indicar se o usuário deve prosseguir
+transitionsTable = []  # Tabela de transições
+transitions = []  # Terminais das transições
+while(not goAhead):
+    print("-------- Preenchimento dos Dados da Tabela de Transições -----")
+    print("*   entradas separadas por vírgual. @ para entradas vazias   *\n")
+    try:
+        actions = input(
+            f"Insira as variáveis({countTransitions}) das transições: ").split(',')
+
+        transitionLine = []
+        for i in range(countStates):
+            transitionLine = input(
+                f"Insira as transições({countTransitions}) da linha {i+1}: ")
+            transitionLine = transitionLine.split(',')
+            transitionsTable.append(transitionLine)
+
+        goAhead = input(
+            "\nPressione 1 para continuar. Qualquer outra tecla para repetir: ")
+        goAhead = goAhead == "1"
+    except:
+        input("\nVocê inseriu algum dado incorreto. Pressione qualquer tecla para repetir: ")
+        goAhead = False
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# 3. Preenchimento da gramática
+# - O preenchimento da gramática utiliza os caracteres '->' para separar os lados esquerdo e direito
+# - O espaço deve ser utilizado para separar cada elemento do lado esquerdo
+# - Exemplo: E -> E + T
+
+goAhead = False
+while(not goAhead):
+    print("---------------------- Preenchimento da Gramática ----------------------")
+    print("*          regra separadas por ->. espaços devem ser utilizados        *\n")
+    try:
+        for i in range(countRules):
+            rule = input(f"Insira a regra {i+1}: ")
+
+            rule = rule.split('->')
+            # Removendo espaçoes em branco
+            rule[0] = rule[0].rstrip()
+            rule[1] = rule[1].strip()
+            rule[1] = rule[1].split(" ")
+            grammar.append(rule)
+
+        goAhead = input(
+            "\nPressione 1 para continuar. Qualquer outra tecla para repetir: ")
+        goAhead = goAhead == "1"
+    except:
+        input(
+            "\nVocê inseriu algum dado incorreto. Pressione qualquer tecla para repetir: ")
+        goAhead = False
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# 4. Entrada da sentença
+# - O espaço deve ser utilizado para separar cada elemento da sentença
+# - Exemplo: id * id + id
+
+sentence = input("Insira a sentença a ser avaliada: ")
+
+# 5. Análise Preditiva
+statesStack = []
+sentenceSimbols = []
+
+# Retorna uma ação da tabela, a partir de uma dada variável de ação(a) e estado(s)
+def getCurrentAction(a,s): 
+    actionIndex = actions.index(a)
+    actionsRow = actionsTable[s]
+    return actionsRow[actionIndex] # retorna a ação encontrada
+
+# Empilha o estado inicial
+statesStack.append('0')
+
+# Recebe o primeiro símbolo da sentença
+actionVar = sentence[0]
 
 while(True):
-    s = pilha[-1] #estado do topo da pilha
+    currentState = statesStack[-1] # Estado no topo da pilha
 
-    acao = vAct(a,s)
+    currentAction = getCurrentAction(actionVar,currentState)
 
-    if(acao[0] == 's'):#siginifica que a acao é um empilhamento(shift)
+    if(currentAction[0] == 's'): # Significa que a ação é um empilhamento (shift)
 
-        pilha.append(acao[1]) #empilha o estado
+        statesStack.append(currentAction[1]) # Empilha o estado
 
-        n = fita.pop(0)#remove o caracter lido da fita
+        processedSimbol = sentence.pop(0) # Remove o simbolo lido da fita
 
-        simbolo.append(n)# coloca na pilha de simbolos
+        simbolsStack.append(processedSimbol) # Adiciona na pilha de simbolos já processados
 
-        a = fita[0]#atualiza o proximo caracter de entrada
+        actionVar = sentence[0] # Atualiza o proximo caracter de entrada
 
-    else if(acao[0] == 'r'):#siginifica que a acao é uma reducão(reduce)
+    elif (currentAction[0] == 'r'): # Siginifica que a acao é uma reducão(reduce)
 
-        numeroRegra = acao[1] #identifica a regra usada para fazer a reducao
-        regra = grammar[numeroRegra] #regra utilizada na reducao
+        # Identificar gancho
+        ruleNumber = currentAction[1] # Identifica a regra usada para fazer a reducao
+        rule = grammar[ruleNumber] # Regra utilizada na reducao
 
+        #substituir gancho na pilha de simbolos
         #remove os ultimos n elementos da pilha de simbolo
         #sendo n a quantidade de caracteres do lado direito da regra especificada
-        simbolo = simbolo[:len(simbolo) - len(regra[1])]
-
+        simbolsStack = simbolsStack[:len(simbolo) - len(regra[1])] #inconsistente?
         #empilha o lado esquerdo da regra
-        simbolo.append(regra[0])
+        simbolsStack.append(rule[0])
 
-        #remove os elementos da pilha
-        pilha = pilha[:len(pilha)-len(regra[1])]
+        # Remove o elemento da pilha
+        statesStack.pop()
 
-        #empilha o novo estado de acordo com a tabela de transicoes
-        topo = pilha[-1]
-        
-        index = transitionsTable.index(regra[0])
+        # Empilha o novo estado de acordo com a Tabela de Transicoes
+        top = statesStack[0]
+        index = transitionsTable.index(rule[0])
+        newState = transitionsTable[top][index]
+        statesStack.append(newState)
 
-        novoEstado = transitionsTable[topo][index]
+        print(rule[0],"->",rule[1])
 
-        pilha.append(novoEstado)
-
-        print(regra[0],"-->",regra[1])
-    else if(acao == "$"):
-        print("fim")
+    elif(currentAction == "$"):
+        print("Fim")
         break
-    else
-        print("erro")   
+    else:
+        print("Erro")   
         break
